838542e9891e382fe91bcfd612ec9632ac7dce90
wloszynski <adrian@wloszynski.pl>
2020-12-29 17:33:10 +0100

    creating a functional component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <Person />
+      </div>
+    );
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hi, I'm a React App!!!")
     // );
-    return React.createElement("div", null);
   }
 }
 
