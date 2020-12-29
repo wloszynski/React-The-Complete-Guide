329dcb48ed1f1118557ecb3b6afa806e450baba9
wloszynski <adrian@wloszynski.pl>
2020-12-29 19:46:48 +0100

    styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -44,6 +44,13 @@ class App extends Component {
   };
 
   render() {
+    // const style = {
+    //   backgroundColor: "white",
+    //   font: "inherit",
+    //   border: "1px solid blue",
+    //   padding: "8px",
+    //   cursor: "pointer",
+    // };
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
