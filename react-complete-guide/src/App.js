459914f6c6d014049a0fa1c1c80c0189ff1f7e7e
wloszynski <adrian@wloszynski.pl>
2020-12-30 11:05:47 +0100

    handling dynamic content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -52,37 +52,37 @@ class App extends Component {
   };
 
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "TimTim")}
+            changed={this.nameChangedHandler}
+          >
+            My Hobbies: Kotkies
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "TimTim")}
-              changed={this.nameChangedHandler}
-            >
-              My Hobbies: Kotkies
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", null, "Hi, I'm a React App!!!")
-    // );
   }
 }
 
