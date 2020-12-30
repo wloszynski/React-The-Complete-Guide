9ef458d383b13191e754e732618eed4dea9d5311
wloszynski <adrian@wloszynski.pl>
2020-12-30 19:32:20 +0100

    config

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import "./App.css";
+// import "./App.css";
+import classes from "./App.css";
 
 import Person from "./Person/Person";
 
@@ -50,6 +51,8 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = [classes.Button];
+
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -68,20 +71,22 @@ class App extends Component {
           })}
         </div>
       );
+      btnClass.push(classes.Red);
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
         <button
+          className={btnClass.join(" ")}
           myAlt={this.state.showPersons}
           onClick={this.togglePersonsHandler}
         >
