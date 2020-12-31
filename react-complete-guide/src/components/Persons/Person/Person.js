f71591eebb51394a7e595c44380fffad0fb343f1
wloszynski <adrian@wloszynski.pl>
2020-12-31 12:46:04 +0100

    refs

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,6 +6,15 @@ import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering...");
 
@@ -17,6 +26,10 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input
           type="text"
+          // ref={inputEl => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           onChange={this.props.changed}
           value={this.props.name}
         />
