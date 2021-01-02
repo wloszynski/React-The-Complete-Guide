29027c4915643b44d68fdb5f82015d7e572b925b
wloszynski <adrian@wloszynski.pl>
2021-01-02 15:51:48 +0100

    adding ingredients dynamically

--- burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 
 import classes from "./BuildControl.css";
 
@@ -8,7 +7,9 @@ const buildControl = (props) => {
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
       <button className={classes.Less}>Less</button>
-      <button className={classes.More}>More</button>
+      <button className={classes.More} onClick={props.added}>
+        More
+      </button>
     </div>
   );
 };
