cdf20591d4727d1bdd6da1fa4329548ebfdc5848
wloszynski <adrian@wloszynski.pl>
2021-01-02 17:10:16 +0100

    removing ingredients safely

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -19,6 +19,10 @@ const buildControls = (props) => (
         added={() => {
           props.ingredientAdded(ctrl.type);
         }}
+        removed={() => {
+          props.ingredientRemoved(ctrl.type);
+        }}
+        disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
