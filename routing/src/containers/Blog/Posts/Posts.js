096d06ed6e19bfb73a16cfa0562f334c4cac3456
wloszynski <adrian@wloszynski.pl>
2021-01-04 13:28:28 +0100

    naviagring programmatically

--- routing/src/containers/Blog/Posts/Posts.js
+++ routing/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "../../../axios";
-import { Link } from "react-router-dom";
 
 import Post from "../../../components/Post/Post";
 
@@ -33,7 +32,7 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: "/" + id });
   };
 
   render() {
@@ -41,13 +40,13 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={"/" + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          // <Link to={"/" + post.id} key={post.id}>
+          <Post
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          // </Link>
         );
       });
     }
