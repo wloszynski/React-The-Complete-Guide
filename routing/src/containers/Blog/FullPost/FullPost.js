8e94b9995c1f7e27bee1a3e0c513421055866b4d
wloszynski <adrian@wloszynski.pl>
2021-01-04 14:01:55 +0100

    redirecting requests

--- routing/src/containers/Blog/FullPost/FullPost.js
+++ routing/src/containers/Blog/FullPost/FullPost.js
@@ -9,10 +9,19 @@ class FullPost extends Component {
   };
 
   componentDidMount() {
+    this.loadData();
+  }
+
+  componentDidUpdate() {
+    this.loadData();
+  }
+
+  loadData() {
     if (this.props.match.params.id) {
       if (
         !this.state.loadedPost ||
-        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+        (this.state.loadedPost &&
+          this.state.loadedPost.id !== +this.props.match.params.id)
       ) {
         axios.get("/posts/" + this.props.match.params.id).then((response) => {
           // console.log(response);
@@ -23,7 +32,7 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete("/posts/" + this.props.id).then((response) => {
+    axios.delete("/posts/" + this.props.match.params.id).then((response) => {
       console.log(response);
     });
   };
