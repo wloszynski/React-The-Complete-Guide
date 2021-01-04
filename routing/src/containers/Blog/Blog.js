0f2ba60ab609bd9a924f4bd79f6a5917a115fb03
wloszynski <adrian@wloszynski.pl>
2021-01-04 13:14:46 +0100

    using switch to load a single route

--- routing/src/containers/Blog/Blog.js
+++ routing/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 // import axios from 'axios';
 import axios from "../../axios";
 import Posts from "./Posts/Posts";
@@ -45,9 +45,11 @@ class Blog extends Component {
         {/* <Posts /> */}
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home2</h1>} /> */}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
