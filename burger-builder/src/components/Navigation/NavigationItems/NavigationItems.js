bc2307c61c3e82512f912809f61999cb8aacc387
wloszynski <adrian@wloszynski.pl>
2021-01-05 15:54:09 +0100

    outputting the orders

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,7 +1,6 @@
 import React from "react";
-
-import classes from "./NavigationItems.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from "./NavigationItems.css";
 
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
