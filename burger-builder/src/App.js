d1a0c5eb8ce0dc6815a397d5b9d1a53b1dac68b7
wloszynski <adrian@wloszynski.pl>
2021-01-05 15:38:26 +0100

    fetching orders

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./components/Orders/Orders";
+import Orders from "./containers/Orders/Orders";
 class App extends Component {
   render() {
     return (
--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,10 +5,10 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>
+    <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link="/">Orders</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
 
