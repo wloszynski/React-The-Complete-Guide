c8256e75f53ab52b9168e7e77777d09a55365cd9
wloszynski <adrian@wloszynski.pl>
2021-01-05 13:12:49 +0100

    navigating to the contact data component

---
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+import Button from "../../../components/UI/Button/Button";
+
+import classes from "./ContactData.css";
+
+class ContactData extends Component {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      postalCode: "",
+    },
+  };
+
+  render() {
+    return (
+      <div className={classes.ContactData}>
+        <h4>Entry your Contact Data</h4>
+        <form>
+          <input
+            className={classes.Input}
+            type="text"
+            name="name"
+            placeholder="Your name"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="email"
+            placeholder="Your email"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="street"
+            placeholder="Street"
+          />
+          <input
+            className={classes.Input}
+            type="text"
+            name="postalCode"
+            placeholder="Postal Code"
+          />
+          <Button btnTYpe="Success">ORDER</Button>
+        </form>
+      </div>
+    );
+  }
+}
+export default ContactData;
