b8488dadac70fa2238f5b711bd077572383ef65c
wloszynski <adrian@wloszynski.pl>
2021-01-06 17:34:18 +0100

    combining local UI state and redux

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -6,25 +6,6 @@ import AddPerson from "../components/AddPerson/AddPerson";
 import * as actionTypes from "../actions";
 
 class Persons extends Component {
-  personAddedHandler = () => {
-    const newPerson = {
-      id: Math.random(), // not really unique but good enough here!
-      name: "Max",
-      age: Math.floor(Math.random() * 40),
-    };
-    this.setState((prevState) => {
-      return { persons: prevState.persons.concat(newPerson) };
-    });
-  };
-
-  personDeletedHandler = (personId) => {
-    this.setState((prevState) => {
-      return {
-        persons: prevState.persons.filter((person) => person.id !== personId),
-      };
-    });
-  };
-
   render() {
     return (
       <div>
