c6e672fdc565ed83d518c3c5f0247444b0629191
wloszynski <adrian@wloszynski.pl>
2021-01-08 11:56:42 +0100

    restructuring actions

---
+++ redux-adv--01-start/src/store/actions/result.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "./actionTypes";
+
+export const saveResult = (res) => {
+  return {
+    type: actionTypes.STORE_RESULT,
+    result: res,
+  };
+};
+
+export const store_result = (res) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(saveResult(res));
+    }, 2000);
+  };
+};
+
+export const delete_result = (id) => {
+  return {
+    type: actionTypes.DELETE_RESULT,
+    resultElId: id,
+  };
+};
